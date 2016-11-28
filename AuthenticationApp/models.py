b2ff59e44bb79771c00d73a460745e7f70d63712
Su <su68@purdue.edu>
2016-11-28 18:02:58 -0500

    piazza change

--- AuthenticationApp/models.py
+++ AuthenticationApp/models.py
@@ -18,6 +18,7 @@ class MyUserManager(BaseUserManager):
         #We can safetly create the user
         #Only the email field is required
         user = self.model(email=email)
+		user.last_name = last_name
         user.set_password(password)
 
         #If first_name is not present, set it as email's username by default
